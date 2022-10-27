f90f5719ea6f5abf608940918f2325d8b2dab27e
Impp18 <tdfallara@gmail.com>
2022-10-27 17:21:22 -0400

    updated and reacfored style sheet. done in one giant coding session, i forgot to push more often

--- script.js
+++ script.js
@@ -1,43 +1,86 @@
 
 
-var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "+", "=", "(", ")", "/", "|", "?"]
-var letterLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var letterUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numeric = [1,2,3,4,5,6,7,8,9,0]
-var answers = []
-
-console.log()
+var specialCharacters = "!@#$%&*+=()/|?";
+var letterLowerCase = "abcdefghijklmnopqrstuvwxyz"
+var letterUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "1234567890"
+var chosenCharacters = ""
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //howLong takes users' input on length of password
-  var howLong = prompt("How long of a password would you like? \nMust be between 8 and 128 characters");
+  var howLong = prompt("How long of a password would you like? \nPlease type a number between 8 and 128 \n(1, 2, or 3, not one two or three)");
   console.log(howLong)
-  //fail state being between 8 and 128
-  if (howLong < 8 || howLong > 128) {
-    alert("Password must be between 8 and 128 characters. \nPlease try again.");
-    return;
-  } 
-  else {
-    var lengthCorrect = confirm("Password will be " + howLong + " characters long. Is that correct?")
-  }
-  console.log(lengthCorrect)
-    //confrim lower case
-      var useLowerCase = confirm("Great! Would you like to use lower case letters in this password?" )
-      console.log(useLowerCase)
-    //confrim uppercase
-      var useUpperCase = confirm("Alright, would you like to use upper case letters?") 
-    console.log(useUpperCase)
-    //confrim numeric
-      var useNumeric = confirm("Cool, would you like to use numbers?") 
-    console.log(useNumeric)
-    //use special characters
-      var useSpecial = confirm("And finally, would you like to use special characters in this password?") 
-    console.log(useSpecial)
 
-}
+  //fail state if click cancel
+  if (howLong == null) {
+      return "Well, bye then."};
+
+  //fail state being between 8 and 128 and if not a number    
+  if (howLong < 8 || howLong > 128 || isNaN(howLong)) {
+    alert("Password must be between 8 and 128 characters and be a number. \nPlease try again.");
+    return "Try again."} 
+
+  //double checks password length
+  if (!confirm("Password will be " + howLong + " characters long. Is that correct?")){ 
+        return generatePassword() }
+
+  //confirm lower case
+  var includeLc = confirm("Great! Would you like to use lower case letters in this password?" );
+  console.log("includeLc = ", includeLc);
+  if (includeLc === true) {
+      chosenCharacters += letterLowerCase;}
+  console.log("chosenCharacters = ", chosenCharacters);
+  
+  //confrim upper case
+    var includeUc = confirm("Great! Would you like to use upper case letters in this password?" );
+
+    console.log("includeUc = ", includeUc);
+    
+    if (includeUc === true) {
+      chosenCharacters += letterUpperCase;}
+    
+      console.log("chosenCharacters = ", chosenCharacters);
+    
+      //confrim numbers
+    var includeNmb = confirm("Great! Would you like to use numbers in this password?" );
+    
+    console.log("includeNmb = ", includeNmb);
+    
+    if (includeNmb === true) {
+      chosenCharacters += numeric;}    
+    
+      console.log("chosenCharacters = ", chosenCharacters);
+    
+      //confrim special characters
+    var includeSpc = confirm("Great! Would you like to use special characters in this password?" );
+    
+    console.log("includeSpc = ", includeSpc);
+    
+    if (includeSpc === true) {
+      chosenCharacters += specialCharacters;}    
+    
+      console.log("chosenCharacters = ", chosenCharacters);
+      //Make sure at lest one character set was chosen
+      if (chosenCharacters.length  === 0){
+        alert("You password must include at least one type of characters to be generated.");
+        return;}
+      
+      //password variables
+      var passkey = "" ;
+      var randomindex = 0;
+      console.log("chosenCharacters length is ", chosenCharacters.length);
+      //generates the password
+      for (var i = 0; i < howLong; i++) {
+        randomindex = Math.floor(Math.random() * chosenCharacters.length)
+        passkey += chosenCharacters[randomindex]
+        console.log("passkey", passkey);}
+      return passkey;
+ 
+      }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -50,9 +93,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//length of more than 8, less than 128
-//ask to include character types
-//lowercase, uppercase, numeric, and/or sepcial characters
-//as each prompt is answered, there should be validation at least one character type should be selected
-//then a password is generated that matches selected criteria
-//password is displayed in an alert or written on the page
